36b4a0b9c5dd2e170e8ea9bf4c1faa5cca45b320
Manu Arora <mannuarora700@gmail.com>
2020-07-07 20:11:14 +0530

    windows output

--- Prepbytes/Easy/Transition Point/code.cpp
+++ Prepbytes/Easy/Transition Point/code.cpp
@@ -1,37 +1,45 @@
 #include <bits/stdc++.h>
-  using namespace std;
-  
-  int findOne(int a[], int start, int end) {
-    if(start > end) return -1;
-    
-    int mid = start + (end-start)/2;
-    
-    if(a[mid] == 1 && (mid == 0 || a[mid-1] == 0)) return mid;
-    else if(a[mid] == 0) return findOne(a, mid+1, end);
-    else return findOne(a, start, mid-1);
-  }
-  
-  int main()
+#include <iostream>
+using namespace std;
+
+int findOne(int a[], int start, int end)
+{
+  if (start > end)
+    return -1;
+
+  int mid = start + (end - start) / 2;
+
+  if (a[mid] == 1 && (mid == 0 || a[mid - 1] == 0))
+    return mid;
+  else if (a[mid] == 0)
+    return findOne(a, mid + 1, end);
+  else
+    return findOne(a, start, mid - 1);
+}
+
+int main()
+{
+  //write your code here
+
+  int t;
+  std::cin >> t;
+
+  while (t--)
   {
-    //write your code here
-    
-    int t;
-    std::cin>>t;
-    
-    while(t--) {
-      int n;
-      std::cin>>n;
-      
-      int a[n];
-      
-      for(int i = 0; i < n; i++) {
-        std::cin>>a[i];
-      }
-      
-      int index = findOne(a, 0, n-1);
-      
-      std::cout<<index<<std::endl;
+    int n;
+    std::cin >> n;
+
+    int a[n];
+
+    for (int i = 0; i < n; i++)
+    {
+      std::cin >> a[i];
     }
-    
-    return 0;
+
+    int index = findOne(a, 0, n - 1);
+
+    std::cout << index << std::endl;
   }
+
+  return 0;
+}
