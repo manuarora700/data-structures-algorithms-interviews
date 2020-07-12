47d8e56004da6bc66d2fb3d01de4f2bc338eebe4
Manu Arora <mannuarora700@gmail.com>
2020-07-12 23:21:46 +0530

    DLL insertion using structures

---
+++ DS Implementations/Linked List/doubleLinkedListStructure.cpp
@@ -0,0 +1,122 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef struct Node
+{
+  int data;
+  struct Node *next;
+  struct Node *prev;
+} Node;
+
+Node *head = NULL;
+void insertAtBeg(int data)
+{
+  Node *newNode = (Node *)malloc(sizeof(Node));
+
+  if (head == NULL)
+  {
+    newNode->data = data;
+    newNode->next = NULL;
+    newNode->prev = NULL;
+
+    head = newNode;
+    return;
+  }
+  newNode->data = data;
+  newNode->next = head;
+  newNode->prev = NULL;
+  head->prev = newNode;
+  head = newNode;
+}
+
+void insertAtEnd(int data)
+{
+  Node *newNode = (Node *)malloc(sizeof(Node));
+
+  newNode->data = data;
+  newNode->next = NULL;
+  newNode->prev = NULL;
+
+  Node *temp = head;
+
+  if (temp == NULL)
+  {
+    head = newNode;
+    return;
+  }
+
+  while (temp->next != NULL)
+  {
+    temp = temp->next;
+  }
+  temp->next = newNode;
+  newNode->prev = temp;
+}
+
+void insertAtPos(int data, int pos)
+{
+  Node *newNode = (Node *)malloc(sizeof(Node));
+
+  newNode->data = data;
+  newNode->next = NULL;
+  newNode->prev = NULL;
+
+  Node *temp = head;
+  int count = 1;
+  if (temp == NULL)
+  {
+    head = newNode;
+  }
+  while (count < pos && temp->next != NULL)
+  {
+    count++;
+    temp = temp->next;
+  }
+  if (temp->next == NULL)
+  {
+    temp->next = newNode;
+    newNode->prev = temp;
+    return;
+  }
+  newNode->next = temp->next;
+  newNode->prev = temp;
+  newNode->next->prev = newNode;
+  temp->next = newNode;
+}
+
+void display()
+{
+  Node *temp;
+  temp = head;
+
+  while (temp != NULL)
+  {
+    std::cout << temp->data << " ";
+    temp = temp->next;
+  }
+}
+int main()
+{
+
+  int t;
+  std::cin >> t;
+
+  while (t--)
+  {
+
+    int n;
+    std::cin >> n;
+
+    int data;
+    for (int i = 0; i < n; i++)
+    {
+      std::cin >> data;
+      insertAtBeg(data);
+    }
+    std::cout << "Insert at the beg: ";
+    display();
+  std:
+    cout << "\n";
+  }
+  return 0;
+}
