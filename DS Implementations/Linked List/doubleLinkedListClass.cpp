3cdcab0cd11d86542e6d855e225f0353a3dfb6b4
Manu Arora <mannuarora700@gmail.com>
2020-07-13 12:57:23 +0530

    Deletion implementation using structure and class

--- DS Implementations/Linked List/doubleLinkedListClass.cpp
+++ DS Implementations/Linked List/doubleLinkedListClass.cpp
@@ -89,4 +89,76 @@ public:
     node->next->prev = node;
     temp->next = node;
   }
+  void deleteAtBeg()
+  {
+    DllNode *temp = head;
+    if (temp == NULL)
+    {
+      head = NULL;
+      std::cout << "LL is empty\n";
+      return;
+    }
+    if (temp->next = NULL)
+    {
+      head = NULL;
+      free(temp);
+      return;
+    }
+    head = head->next;
+    head->prev = NULL;
+    temp->next = NULL;
+    free(temp);
+  }
+  void deleteAtEnd()
+  {
+    DllNode *temp = head;
+
+    if (temp == nullptr)
+    {
+      std::cout << "List empty";
+      return;
+    }
+    if (temp->next == NULL)
+    {
+      head = NULL;
+      free(temp);
+      return;
+    }
+
+    while (temp->next->next != NULL)
+    {
+      temp = temp->next;
+    }
+    DllNode *temp1 = temp->next;
+    temp->next = NULL;
+    temp1->prev = NULL;
+    free(temp1);
+  }
+  void deleteAtPos(int pos)
+  {
+    DllNode *temp = head;
+    if (temp == nullptr)
+    {
+      std::cout << "LL is empty";
+      return;
+    }
+    if (temp->next == NULL)
+    {
+      head = NULL;
+      free(temp);
+      return;
+    }
+    int count = 1;
+
+    while (count < pos - 1)
+    {
+      count++;
+      temp - temp->next;
+    }
+    DllNode *temp1 = temp->next;
+    temp->next = temp1->next;
+    temp1->next->prev = temp;
+    temp1->next = NULL;
+    temp->prev = NULL;
+  }
 }
