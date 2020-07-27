1b4db8d54ef4a6455a6569cf404eec9ff8deb524
Manu Arora <mannuarora700@gmail.com>
2020-07-27 16:15:52 +0530

    min heap and heapify down algorithms

---
+++ DS Implementations/Heap/min-heap-bottom-up.cpp
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int parent(int i)
+{
+  return ((i - 1) / 2);
+}
+
+int left(int i)
+{
+  return (2 * i + 1);
+}
+int right(int i)
+{
+  return (2 * i + 2);
+}
+
+void heapify_down(int heap[], int n, int i)
+{
+  int l = left(i);
+  int r = right(i);
+
+  int s = i;
+  if (l < n && heap[l] < heap[i])
+    s = l;
+  if (r < n && heap[r] < heap[s])
+    s = r;
+
+  if (s != i)
+  {
+    swap(heap[i], heap[s]);
+    heapify_down(heap, n, s);
+  }
+}
+
+void build_heap(int heap[], int n)
+{
+  for (int i = (n / 2 - 1); i >= 0; i--)
+  {
+    heapify_down(heap, n, i);
+  }
+}
+
+void print_heap(int heap[], int n)
+{
+  for (int i = 0; i < n; i++)
+  {
+    cout << heap[i] << " ";
+  }
+  cout << endl;
+}
+
+int size;
+int main()
+{
+  int t;
+  cin >> t;
+
+  while (t--)
+  {
+    size = 0;
+    int n;
+    cin >> n;
+
+    int heap[n];
+
+    for (int i = 0; i < n; i++)
+    {
+      cin >> heap[i];
+      size++;
+    }
+
+    build_heap(heap, n);
+    print_heap(heap, n);
+  }
+
+  return 0;
+}
