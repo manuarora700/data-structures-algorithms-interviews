2641b88edf2cdd7ff7fb0de4c3c5a797ace7daeb
Manu Arora <mannuarora700@gmail.com>
2020-08-07 21:37:17 +0530

    Small optimization

--- Interview Algorithms/Easy/Bubble Sort/code.cpp
+++ Interview Algorithms/Easy/Bubble Sort/code.cpp
@@ -1,3 +1,5 @@
+// Time: O(n^2) | Space: O(1)
+
 #include <bits/stdc++.h>
 
 using namespace std;
@@ -6,20 +8,26 @@ vector<int> bubbleSort(vector<int> array)
 {
   for (int i = 0; i < array.size(); i++)
   {
+    int swapVal = 0;
     for (int j = i + 1; j < array.size(); j++)
     {
       if (array[j] > array[j + 1])
       {
         swap(array[j], array[i]);
+        swapVal = 1;
       }
     }
+    if (swapVal == 0)
+    {
+      // lil optimization
+      for (int i = 0; i < array.size(); i++)
+      {
+        cout << array[i] << " ";
+      }
+      cout << endl;
+      return array;
+    }
   }
-
-  for (int i = 0; i < array.size(); i++)
-  {
-    cout << array[i] << " ";
-  }
-  cout << endl;
 }
 
 int main()
