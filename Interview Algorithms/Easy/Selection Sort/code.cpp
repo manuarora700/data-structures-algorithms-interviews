dca4ee2ae460959f65aa794ebee9b52f0e1eed01
Manu Arora <mannuarora700@gmail.com>
2020-08-07 22:00:12 +0530

    Selection sort

---
+++ Interview Algorithms/Easy/Selection Sort/code.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> selectionSort(vector<int> array)
+{
+  if (array.empty())
+  {
+    return {};
+  }
+  int startIdx = 0;
+  while (startIdx < array.size() - 1)
+  {
+    int smallestIdx = i;
+
+    for (int i = startIdx + 1; i < array.size(); i++)
+    {
+      if (array[smallestIdx] > array[i])
+      {
+        smallestIdx = i;
+      }
+    }
+    swap(array[startIdx], array[smallestIdx]);
+    startIdx++;
+  }
+}
---
+++ Interview Algorithms/Easy/Selection Sort/problem.md
@@ -0,0 +1,9 @@
+## Selection Sort
+
+#### Problem Statement
+
+Write a function that takes in an array of integers and returns a sorted version of that array. Use the Selection Sort algorithm to sort the array.
+
+Sample input: [8, 5, 2, 9, 5, 6, 3]
+
+Sample output: [2, 3, 5, 5, 6, 8, 9]
