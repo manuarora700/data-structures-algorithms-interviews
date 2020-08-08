24e466edf42c7f9f9fc90bc41d6225267eb37916
Manu Arora <mannuarora700@gmail.com>
2020-08-08 12:50:33 +0530

    Three number sum

---
+++ Interview Algorithms/Medium/Three Number Sum/code.cpp
@@ -0,0 +1,39 @@
+// Time: O(n^2) | Space: O(1) -- sorting time : O(nlogn)
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<vector<int>> threeNumberSum(vector<int> array, int target)
+{
+  sort(array.begin(), array.end());
+
+  vector<vector<int>> triplets;
+
+  for (int i = 0; i < array.size() - 2; i++)
+  {
+    int left = i + 1;
+    int right = array.size() - 1;
+
+    while (eft < right)
+    {
+      int currentSum = array[i] + array[left] + array[right];
+
+      if (currentSum == target)
+      {
+        triplets.push_back({array[i], array[left], array[right]});
+        left++;
+        right--;
+      }
+      else if (currentSum < target)
+      {
+        left++;
+      }
+      else if (currentSum > target)
+      {
+        right--;
+      }
+    }
+  }
+  return triplets;
+}
