3b691783df163abf36914e025760acc20e9438a4
Manu Arora <mannuarora700@gmail.com>
2020-08-16 21:57:15 +0530

    Recursive bug fix

--- Interview Algorithms/Medium/powerset/code-recursive.cpp
+++ Interview Algorithms/Medium/powerset/code-recursive.cpp
@@ -1,51 +1,22 @@
-#include <bits/stdc++.h>
+#include <vector>
 using namespace std;
 vector<vector<int>> powersetHelper(vector<int> array, int idx);
 // O(n*2^n) time | O(n*2^n) space
-vector<vector<int>> powerset(vector<int> array)
-{
-  return powersetHelper(array, array.size() - 1);
+vector<vector<int>> powerset(vector<int> array) {
+return powersetHelper(array, array.size() - 1);
 }
-vector<vector<int>> powersetHelper(vector<int> array, int idx)
-{
-  if (idx < 0)
-  {
-    return vector<vector<int>>{{}};
-  }
-  int ele = array[idx];
-  vector<vector<int>> subsets = powersetHelper(array, idx - 1);
-  int length = subsets.size();
-  for (int i = 0; i < length; i++)
-  {
-    vector<int> currentSubset = subsets[i];
-    vector<int> newSubset = currentSubset;
-    newSubset.push_back(ele);
-    subsets.push_back(newSubset);
-  }
-  return subsets;
+vector<vector<int>> powersetHelper(vector<int> array, int idx) {
+if (idx < 0) {
+return vector<vector<int>>{{}};
 }
-#include <bits/stdc++.h>
-using namespace std;
-vector<vector<int>> powersetHelper(vector<int> array, int idx);
-// O(n*2^n) time | O(n*2^n) space
-vector<vector<int>> powerset(vector<int> array)
-{
-  return powersetHelper(array, array.size() - 1);
+int ele = array[idx];
+vector<vector<int>> subsets = powersetHelper(array, idx - 1);
+int length = subsets.size();
+for (int i = 0; i < length; i++) {
+vector<int> currentSubset = subsets[i];
+vector<int> newSubset = currentSubset;
+newSubset.push_back(ele);
+subsets.push_back(newSubset);
 }
-vector<vector<int>> powersetHelper(vector<int> array, int idx)
-{
-  if (idx < 0)
-  {
-    return vector<vector<int>>{{}};
-  }
-  int ele = array[idx];
-  vector<vector<int>> subsets = powersetHelper(array, idx - 1);
-  int length = subsets.size();
-  for (int i = 0; i < length; i++)
-  {
-    vector<int> currentSubset = subsets[i];
-    vector<int> newSubset = currentSubset;
-    newSubset.push_back(ele);
-    subsets.push_back(newSubset);
-  }
-  
+return subsets;
+}
